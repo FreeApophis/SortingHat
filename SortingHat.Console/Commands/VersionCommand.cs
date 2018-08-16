96ba6f29ff423ad319af02c0e08dc67a3466778f
Thomas Bruderer <apophis@apophis.ch>
2018-08-16 10:22:02 +0200

    Help resource for each command

---
+++ SortingHat.Console/Commands/VersionCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SortingHat.CLI.Commands
+{
+    class VersionCommand : ICommand
+    {
+        public bool Execute(IEnumerable<string> arguments)
+        {
+            var version = GetVersion();
+
+            Console.WriteLine($"{version.FullName}");
+
+            return true;
+        }
+
+        private static AssemblyName GetVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName();
+        }
+
+        public string LongCommand => "version";
+        public string ShortCommand => "v";
+        public string ShortHelp => "Shows the current Version of this program";
+    }
+}
