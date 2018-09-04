1dc89c46a7fb27c6ed2c5b81613bce47ed3b1e32
Thomas Bruderer <apophis@apophis.ch>
2018-09-04 22:16:27 +0200

    Replace Setup with a WIX-Setup project

---
+++ ExifTaggerPlugin/ExifTaggerModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using SortingHat.API.DI;
+using System;
+
+namespace ExifTaggerPlugin
+{
+    class ExifTaggerModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            Console.WriteLine("Exif Tagger Module Loading...");
+            builder.RegisterType<ExifTaggerCommand>().As<ICommand>();
+        }
+    }
+}
