8b0dafa40f9539c09fcc4fb918459dd363723c40
Thomas Bruderer <apophis@apophis.ch>
2018-10-05 22:12:56 +0200

    Unused AutoTag -> Replaced with CreatedAt and FilePath

--- SortingHat.API/AutoTag/CreatedAtAutoTag.cs
+++ SortingHat.API/AutoTag/CreatedAtAutoTag.cs
@@ -2,34 +2,33 @@
 using System.IO;
 using System.Linq;
 using JetBrains.Annotations;
-using SortingHat.API.DI;
 
 namespace SortingHat.API.AutoTag
 {
     [UsedImplicitly]
     public class CreatedAtAutoTag : IAutoTag
     {
-        private readonly List<IDateTagPart> _dateParts = new List<IDateTagPart>() { new YearPart(), new MonthPart(), new DayPart() };
-        private readonly string _tagName = "CreatedAt";
+        private readonly List<IDateTagPart> _dateParts = new List<IDateTagPart>() { new YearPart(), new MonthPart(), new DayPart(), new HourPart() };
+        private const string TagName = "CreatedAt";
+
+        private readonly List<string> _possibleAutoTags = new List<string>();
+        public IEnumerable<string> PossibleAutoTags => _possibleAutoTags;
 
         public CreatedAtAutoTag()
         {
-            _possibleAutoTags.InsertRange(0, _dateParts.Select(datePart => TagName(datePart)));
+            _possibleAutoTags.InsertRange(0, _dateParts.Select(TagNameWithDatePart));
         }
 
-        private string TagName(IDateTagPart datePart)
+        private string TagNameWithDatePart(IDateTagPart datePart)
         {
-            return $"{_tagName}.{datePart.Key}";
+            return $"{TagName}.{datePart.Key}";
         }
-
-        private List<string> _possibleAutoTags = new List<string>();
-        public IEnumerable<string> PossibleAutoTags => _possibleAutoTags;
 
         public string HandleTag(string autoTag, string fileName)
         {
             var fileInfo = new FileInfo(fileName);
 
-            return _dateParts.Where(datePart => TagName(datePart) == autoTag).First().Select(fileInfo.CreationTime);
+            return _dateParts.First(datePart => TagNameWithDatePart(datePart) == autoTag).Select(fileInfo.CreationTime);
         }
     }
 }
---
+++ SortingHat.API/AutoTag/FilePathAutoTag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+
+namespace SortingHat.API.AutoTag
+{
+    [UsedImplicitly]
+    public class FilePathAutoTag : IAutoTag
+    {
+        private readonly List<string> _possibleAutoTags = new List<string>();
+        public IEnumerable<string> PossibleAutoTags => _possibleAutoTags;
+
+        public FilePathAutoTag()
+        {
+            _possibleAutoTags.Add("Path.Recursive");
+            _possibleAutoTags.Add("Path.Left");
+            _possibleAutoTags.Add("Path.Right");
+        }
+
+        public string HandleTag(string autoTag, string fileName)
+        {
+            var fileInfo = new FileInfo(fileName);
+
+            var pathElements = PathHelper.PathElements(fileInfo.Directory);
+
+            return null;
+        }
+
+
+    }
+}
