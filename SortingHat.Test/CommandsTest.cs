a1505e6345a67bb50e6101e105e9a5c83be5c37c
Thomas Bruderer <apophis@apophis.ch>
2019-04-11 21:22:36 +0200

    Remove redundant parenthesis

--- SortingHat.Test/CommandsTest.cs
+++ SortingHat.Test/CommandsTest.cs
@@ -12,8 +12,8 @@ namespace SortingHat.Test
             var tagParser = new MockTagParser();
             var addTag = new AddTagCommand(tagParser);
 
-            List<string> taxPeriods = new List<string>() { "tags", "add", ":tax_period:2016", ":tax_period:2017", ":tax_period:2018", ":tax_period:2019" };
-            List<string> movieRating = new List<string>() { "tags", "add", ":movie:bad", ":movie:average", ":movie:good", ":movie:great" };
+            List<string> taxPeriods = new List<string> { "tags", "add", ":tax_period:2016", ":tax_period:2017", ":tax_period:2018", ":tax_period:2019" };
+            List<string> movieRating = new List<string> { "tags", "add", ":movie:bad", ":movie:average", ":movie:good", ":movie:great" };
 
             addTag.Execute(taxPeriods, new Mock.MockOptions());
             addTag.Execute(movieRating, new Mock.MockOptions());
