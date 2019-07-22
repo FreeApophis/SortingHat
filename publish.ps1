5823a0acd5df3394ab6c100c2fa28ea83c9c3972
Thomas Bruderer <apophis@apophis.ch>
2019-07-22 20:38:21 +0200

    Update publish power shell script

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,13 @@
-&dotnet publish SortingHat.Console/SortingHat.CLI.csproj --configuration Release --runtime win-x64 --output ../publish
-&dotnet publish SortingHat.Plugin.Exif/SortingHat.Plugin.Exif.csproj --configuration Release --runtime win-x64 --output ../publish/plugins
-&dotnet publish SortingHat.Plugin.FileType/SortingHat.Plugin.FileType.csproj --configuration Release --runtime win-x64 --output ../publish/plugins
-&warp-packer --arch windows-x64 --input_dir publish --exec hat.exe --output hat.exe
+&dotnet publish SortingHat.Console/SortingHat.CLI.csproj --configuration Release --runtime win-x64 --output publish /p:PublishSingleFile=true
+
+New-Item -Path "publish" -Name "plugins" -ItemType "directory" -Force
+
+&dotnet publish SortingHat.Plugin.Exif/SortingHat.Plugin.Exif.csproj --configuration Release --runtime win-x64
+Copy-Item "SortingHat.Plugin.Exif\bin\Release\netstandard2.1\win-x64\publish\SortingHat.Plugin.Exif.dll" -Destination "publish\plugins"
+Copy-Item "SortingHat.Plugin.Exif\bin\Release\netstandard2.1\win-x64\publish\MetadataExtractor.dll" -Destination "publish\plugins"
+
+&dotnet publish SortingHat.Plugin.FileType/SortingHat.Plugin.FileType.csproj --configuration Release --runtime win-x64
+Copy-Item "SortingHat.Plugin.FileType\bin\Release\netstandard2.1\win-x64\publish\SortingHat.Plugin.FileType.dll" -Destination "publish\plugins"
+
+&dotnet publish SortingHat.Plugin.ExtractRelevantText/SortingHat.Plugin.ExtractRelevantText.csproj --configuration Release --runtime win-x64
+Copy-Item "SortingHat.Plugin.ExtractRelevantText\bin\Release\netstandard2.1\win-x64\publish\SortingHat.Plugin.ExtractRelevantText.dll" -Destination "publish\plugins"
