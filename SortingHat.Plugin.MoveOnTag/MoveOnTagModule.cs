eec87153ce28306cfc48672e535de0d9da2f7cf9
Thomas Bruderer <apophis@apophis.ch>
2019-08-28 00:56:25 +0200

    New Plugin MoveOnTag added

---
+++ SortingHat.Plugin.MoveOnTag/MoveOnTagModule.cs
@@ -0,0 +1,17 @@
+﻿using SortingHat.API.Plugin;
+using System;
+using System.Reflection;
+using JetBrains.Annotations;
+
+namespace SortingHat.Plugin.MoveOnTag
+{
+    [UsedImplicitly]
+    public class MoveOnTagModule : Autofac.Module, IPlugin
+    {
+        public string Name => "Move on tag";
+
+        public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public string Description => "This plugin can automatically move a file, when a certain tag-mask is set for the file.";
+    }
+}
